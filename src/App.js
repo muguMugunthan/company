c54a3d5df92ea80a5201701d476a7c1f08981455
muguMugunthan <50227435+muguMugunthan@users.noreply.github.com>
2020-09-14 21:22:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import logo from './logo.png';
 import './App.css';
-import Form from './components/Form'
-
-
+import {FaCheckCircle,AiOutlineFileProtect} from "react-icons/fa"
+import {FaAngellist} from "react-icons/fa"
 function App() {
   return (
-
     <div className="App">
-      <Form />
-
+     <img src={logo} alt="wev"  align='left'/>
+    <div className="co">
+      <h1> What is Coronavirus?</h1><br/>
+    larem ipsum dolor sit amet,consextextur adipscing elit.Ex<br/>
+      officia quas, modi sit eligendi numquam!<br/><br/>
+     
+      <FaCheckCircle size='20px' color='blue' /> lorem ipsum dolar sit amet<br/>
+     <FaCheckCircle size='20px' color='blue'/>consectetur adipisicing elit<br/>
+      <FaCheckCircle size='20px' color='blue'/>unde doloremqe<br/><br/><br/>
+            <button>LEARN MORE</button><br/>
+      <div className="last">
+      
+       <FaAngellist size='50px' color='blue'/><b>Protection</b>&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; 
+       <FaAngellist size='50px' color='blue'/><b>Prevention</b>&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; 
+       <FaAngellist size='50px' color='blue'/><b>Treatments</b>&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; 
+        <FaAngellist size='50px' color='blue'/><b>Symptoms</b> <br/>
+       &emsp;&emsp; loremipsum dolor sit &emsp;&emsp;&emsp;&emsp;&emsp;loremipsum dolor sit&emsp;&emsp;&emsp;&emsp;&emsp; loremipsum dolor sit&emsp;&emsp;&emsp;&emsp;&emsp;loremipsum dolor sit 
     </div>
-
+    </div>
+    </div>
+    
   );
 }
 
