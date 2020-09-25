449045cd59a115cd9aa4186b4e7e9a1aeda965ba
muguMugunthan <50227435+muguMugunthan@users.noreply.github.com>
2020-09-25 10:59:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,163 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form'
+import {Container,Row,Col} from "react-bootstrap";
+import logo from './logo.jpg'
+
+class RegisterForm extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        fields: {},
+        errors: {}
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+      this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
+
+    };
+
+    handleChange(e) {
+      let fields = this.state.fields;
+      fields[e.target.name] = e.target.value;
+      this.setState({
+        fields
+      });
+
+    }
+
+    submituserRegistrationForm(e) {
+      e.preventDefault();
+      if (this.validateForm()) {
+          let fields = {};
+          fields["username"] = "";
+          fields["emailid"] = "";
+          fields["mobileno"] = "";
+          fields["password"] = "";
+          fields["repassword"]="";
+          this.setState({fields:fields});
+          alert(fields["username"]+"Successfully Registered");
+      }
+
+    }
+
+    validateForm() {
+
+      let fields = this.state.fields;
+      let errors = {};
+      let formIsValid = true;
+
+      if (!fields["username"]) {
+        formIsValid = false;
+        errors["username"] = "*Please enter your username.";
+      }
+
+      if (typeof fields["username"] !== "undefined") {
+        if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+          formIsValid = false;
+          errors["username"] = "*Please enter alphabet characters only.";
+        }
+      }
+
+      if (!fields["emailid"]) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter your email-ID.";
+      }
+
+      if (typeof fields["emailid"] !== "undefined") {
+        //regular expression for email validation
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(fields["emailid"])) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter valid email-ID.";
+        }
+      }
+
+      if (!fields["mobileno"]) {
+        formIsValid = false;
+        errors["mobileno"] = "*Please enter your mobile no.";
+      }
+
+      if (typeof fields["mobileno"] !== "undefined") {
+        if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
+          formIsValid = false;
+          errors["mobileno"] = "*Please enter valid mobile no.";
+        }
+      }
+
+      if (!fields["password"]) {
+        formIsValid = false;
+        errors["password"] = "*Please enter your password.";
+      }
+
+      if (typeof fields["password"] !== "undefined") {
+        if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+          formIsValid = false;
+          errors["password"] = "*Please enter secure and strong password.";
+        }
+      }
+
+      if (!fields["repassword"]) {
+        formIsValid = false;
+        errors["repassword"] = "*Please repeat your password.";
+      }
+
+      if ( fields["repassword"] !== fields["password"]) {
+    
+       formIsValid = false;
+        errors["repassword"] = "*Please enter the same password.";
+        
+      }
+      this.setState({
+        errors: errors
+      });
+      return formIsValid;
 
 
-function App() {
-  return (
+    }
 
-    <div className="App">
-      <Form />
 
+
+  render() {
+    return (
+        <div id="main-registration-container">
+     
+       
+       <div id="register">
+      
+       <img src={logo} />
+       <div className="fir">Welcome to Accelevnts</div>
+       <h4>&emsp;&emsp;&emsp;&emsp; Get started with a free account</h4>
+        
+        <form method="post"  name="userRegistrationForm"  onSubmit= {this.submituserRegistrationForm} >
+        
+        <input type="text" name="username" value={this.state.fields.username} onChange={this.handleChange} placeholder="Visitors Name" />
+        <div className="errorMsg">{this.state.errors.username}</div>
+        
+        <input type="text" name="mobileno" value={this.state.fields.mobileno} onChange={this.handleChange} placeholder="Mobile No"  />
+       
+        <div className="errorMsg">{this.state.errors.mobileno}</div>
+        
+        <input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}placeholder="Email"  />
+        <div className="errorMsg">{this.state.errors.emailid}</div>
+        
+        <input type="password" name="password" value={this.state.fields.password} onChange={this.handleChange} placeholder="Create Password"/>
+        <div className="errorMsg">{this.state.errors.password}</div>
+        
+        <input type="password" name="repassword" value={this.state.fields.repassword} onChange={this.handleChange} placeholder="Repeat Password"/>
+        <div className="errorMsg">{this.state.errors.repassword}</div>
+        <input type="submit" className="button"  value="Get Started"/>
+        <div className="last">By singning up,I agree to Accelevent's terms of service and privacy policy</div>
+        </form>
+     
     </div>
+    </div>
+       
+           
+      );
+  }
 
-  );
+
 }
 
-export default App;
+
+export default RegisterForm;
